e6c7146460f9bb3e71821a40e19266a540a3475b
pankaj kumar <komal9931@users.noreply.github.com>
2024-09-13 22:33:57 +0530

    first commit

---
+++ new-python project/curdoperation/tweet/urls.py
@@ -0,0 +1,24 @@
+
+# from django.contrib import admin
+
+# from django.conf import settings
+# from django.conf.urls.static import static
+
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    # path('',views.index,name='index'),
+    path('',views.tweet_list,name='tweet_list'),
+
+    path('create/',views.tweet_create,name='tweet_create'),
+
+    path('<int:tweet_id>/edit/',views.tweet_edit,name='tweet_edit'),
+
+    path('<int:tweet_id>/delete/',views.tweet_delete,name='tweet_delete'),
+    # path('tweet/', include('tweet.urls')),
+    #...
+    #...
+] 
